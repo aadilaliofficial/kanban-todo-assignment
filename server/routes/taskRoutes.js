1ff72d62aee827b4f815ff0b18a23fbcd1166ec3
Aadil Ali <61555486+aadilaliofficial@users.noreply.github.com>
2025-07-17 16:25:59 +0530

    Update taskRoutes.js

--- server/routes/taskRoutes.js
+++ server/routes/taskRoutes.js
@@ -5,6 +5,7 @@ const {
   createTask,
   updateTask,
   deleteTask,
+  smartAssign,
 } = require('../controllers/taskController');
 
 const { authMiddleware } = require('../middlewares/authMiddleware');
@@ -13,9 +14,28 @@ router.get('/', authMiddleware, getTasks);
 router.post('/', authMiddleware, createTask);
 router.put('/:id', authMiddleware, updateTask);
 router.delete('/:id', authMiddleware, deleteTask);
+router.post('/:id/smart-assign', authMiddleware, smartAssign);
 
 module.exports = router;
 
-const { smartAssign } = require('../controllers/taskController');
+// const express = require('express');
+// const router = express.Router();
+// const {
+//   getTasks,
+//   createTask,
+//   updateTask,
+//   deleteTask,
+// } = require('../controllers/taskController');
 
-router.post('/:id/smart-assign', authMiddleware, smartAssign);
+// const { authMiddleware } = require('../middlewares/authMiddleware');
+
+// router.get('/', authMiddleware, getTasks);
+// router.post('/', authMiddleware, createTask);
+// router.put('/:id', authMiddleware, updateTask);
+// router.delete('/:id', authMiddleware, deleteTask);
+
+// module.exports = router;
+
+// const { smartAssign } = require('../controllers/taskController');
+
+// router.post('/:id/smart-assign', authMiddleware, smartAssign);
